241459648ad94dfef8899d4f05c49a5ddd9863b0
OakleafOne <oliver.ekbladd@gmail.com>
2026-05-26 00:29:31 +0200

    Added the events to the portfolio.

--- my-react-app/src/pages/portfolio.jsx
+++ my-react-app/src/pages/portfolio.jsx
@@ -5,6 +5,85 @@ import ProjectPopup from "../components/ProjectPopup";
 
 const Portfolio = () => {
     const [preview, setPreview] = useState(false);
-    
+    const [previewButton, setPreviewButton] = useState("Show Projects");
+    const [search, setSearch] = useState("");
+    const [popup, setPopup] = useState(false);
+    const [popupData, setPopupData] = useState({});
+
+    const projects = [
+        {
+            title: "Project: My first website",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "Project: SVG",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "Project: Form",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "Project: Grid",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "Project: Flexbox",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "Project: JavaScript variables and operators",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "",
+            tags: "",
+            info: "",
+        },
+        {
+            title: "",
+            tags: "",
+            info: "",
+        },
+    ];
+
+    const showPopup = (index) => {
+        setPopup(true);
+        setPopupData({
+            title: projects[index].title,
+            tags: projects[index].tags,
+            info: projects[index].info,
+        });
+    };
+
+    const closePopup = () => {
+        setPopup(false);
+    };
+
+    const togglePreview = () => {
+        setPreview(!preview);
+        if (!preview) {
+            setPreviewButton("Hide Projects");
+        } else {
+            setPreviewButton("Show Projects");
+        }
+    };
+};  
 
 export default Portfolio;
